28be16dfdf97c933c07eab30b31b1a607e00012b
ThanhLuan1810 <5951071054@st.utc2.edu.vn>
2021-03-22 10:37:49 +0700

    API
    Phần 1: Website gọi Web Service & Restful API

---
+++ KhuatLeThanhLuan_5951071054/KhuatLeThanhLuan_5951071054/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KhuatLeThanhLuan_5951071054.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ KhuatLeThanhLuan_5951071054/KhuatLeThanhLuan_5951071054/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KhuatLeThanhLuan_5951071054
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
